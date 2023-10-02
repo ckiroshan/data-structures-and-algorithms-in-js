102115aa147f6f221f44287a21e695b2502a4d24
Your <ckiroshan@gmail.com>
2023-10-02 15:51:40 +0530

    Snippet: Bubble sort explained.

--- Snippets/Sorting Algorithms/bubbleSort.js
+++ Snippets/Sorting Algorithms/bubbleSort.js
@@ -34,3 +34,35 @@ function bubbleSort(array) {
  * 6. Return array
  */
 
+// <===== Commented Code =====>
+
+// Create an array with random values.
+// let randomArray = Array.from({ length: 6 }, () => Math.floor(Math.random() * 20));
+
+function sort1(array) {
+  let swapped = true; // Check If array was swapped
+  let passes = 0; // Track pass throughs done
+  console.log("Unsorted Array: ", array);
+
+  while (swapped) {
+    // While swapped: true
+    swapped = false; // Reset swapped to false at start
+
+    for (let i = 0; i < array.length - 1; i++) {
+      // Loop through the array
+      if (array[i] > array[i + 1]) {
+        // If the current element is > than the next element
+        [array[i], array[i + 1]] = [array[i + 1], array[i]];
+        // Swap the elements
+        swapped = true;
+        // Set swapped to true to indicate a swap occurred in this pass
+      }
+    }
+    passes++; // Increment the passes variable by 1.
+  }
+
+  console.log("Pass-throughs: ", passes);
+  return array;
+}
+
+// console.log("Sorted Array: ", sort1(randomArray));
